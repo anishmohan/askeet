14f7c04165cc8a9a7756807bdd118d37227b9673
Anish Mohan <anish.aqua@gmail.com>
2012-08-05 14:08:15 +0530

    symfony project created
    Symfony project created
    Symfony frontend webapp created

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'C:\wamp\bin\php\php5.2.6\PEAR\symfony/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+  }
+}
